00676877e1f3b8ea0935ab8206b2c976a2c669a4
queenkjuul <queenkjuul@pm.me>
2025-12-14 22:43:42 -0600

    fix: correct routing behind reverse-proxy prefix

--- main.go
+++ main.go
@@ -24,7 +24,15 @@ func main() {
 	cert := flag.String("cert", "cert.pem", "certificate file to use (if TLS enabled)")
 	key := flag.String("key", "key.pem", "key file to use (if TLS enabled)")
 	id := flag.Int64("id", -1, "number of the specific rule you'd like to retrieve")
+	route := flag.String("route", "/", "route, including prefix, that server will listen on (such as when behind a reverse proxy, for example '/ferengi')")
 	flag.Parse()
+
+	var prefix string
+	if *route == "/" {
+		prefix = ""
+	} else {
+		prefix = *route
+	}
 
 	data, err := f.ReadFile("ferengi.json")
 	if err != nil {
@@ -65,14 +73,25 @@ func main() {
 		// -serve flag, REST API mode
 		mux := http.NewServeMux()
 
-		mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		var defaultResponse = func(w http.ResponseWriter, _ *http.Request) {
 			i = rand.Int63n(int64(len(m)))
 			w.Write([]byte(rules[i]))
-		})
-		mux.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
+		}
+
+		// I have mine set up behind a reverse proxy, where queenkjuul.xuz/ferengi -> localhost:8003
+		// caddy passes through /ferengi as the path... I don't want to set up a trailing slash redirect in Caddy.
+		// if I don't register both patterns here, then one or the other (with or without trailing slash) will 404.
+		// if you know a better way to handle this (where both with+without trailing slash always match, but id param is still handled)
+		// please open an Issue or PR <3
+		if *route != "/" {
+			mux.HandleFunc("GET "+*route+"/", defaultResponse)
+		}
+		mux.HandleFunc("GET "+*route, defaultResponse)
+		mux.HandleFunc("GET "+prefix+"/{id}", func(w http.ResponseWriter, r *http.Request) {
 			val, err := strconv.Atoi(r.PathValue("id"))
 			if err != nil {
 				http.Error(w, "Error processing provided ID", http.StatusInternalServerError)
+				log.Println(err)
 				return
 			}
 			i = int64(val)
@@ -80,15 +99,16 @@ func main() {
 				w.Write([]byte(v))
 			} else {
 				http.Error(w, "Not Found", http.StatusNotFound)
+				log.Println(err)
 				return
 			}
 		})
 
 		if *tls {
-			fmt.Printf("HTTPS server listening on %v\n", *address)
+			fmt.Printf("HTTPS server listening on %v at %v\n", *address, *route)
 			log.Fatal(http.ListenAndServeTLS(*address, *cert, *key, mux))
 		} else {
-			fmt.Printf("HTTP server listening on %v\n", *address)
+			fmt.Printf("HTTP server listening on %v at %v\n", *address, *route)
 			log.Fatal(http.ListenAndServe(*address, mux))
 		}
 	}
